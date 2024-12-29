61f4ebd2664a36ee582880b34e70561a260e8a30
Marcin Leliwa <marcin.leliwa@gmail.com>
2024-12-29 22:31:33 +0100

    0.1.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="ampf",
-    version="0.1.3",
+    version="0.1.4",
     packages=find_packages(),
     install_requires=["fastapi", "pydantic"],
     author="Marcin Leliwa",
