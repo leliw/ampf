fa95b7b7aff115c67abae1d8cb4a2ff6c37b6c02
Marcin Leliwa <marcin.leliwa@gmail.com>
2025-03-22 21:39:21 +0100

    v. 0.1.18

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="ampf",
-    version="0.1.17",
+    version="0.1.18",
     packages=find_packages(),
     install_requires=["fastapi", "pydantic"],
     extras_require={
