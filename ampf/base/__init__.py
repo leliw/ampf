49185ec67afae907867bd7e51b8b9233e7c5376b
Marcin Leliwa <marcin.leliwa@gmail.com>
2025-04-30 20:15:55 +0200

    refactor

--- ampf/base/__init__.py
+++ ampf/base/__init__.py
@@ -5,6 +5,7 @@ from .base_collection_storage import BaseCollectionStorage
 from .base_decorator import BaseDecorator
 from .base_email_sender import BaseEmailSender
 from .base_factory import BaseFactory, CollectionDef
+from .base_query import BaseQuery
 from .base_storage import BaseStorage
 from .email_template import EmailTemplate
 from .exceptions import KeyExistsException, KeyNotExistsException
@@ -15,6 +16,7 @@ __all__ = [
     "BaseFactory",
     "BaseAsyncFactory",
     "BaseStorage",
+    "BaseQuery",
     "BaseAsyncStorage",
     "KeyExistsException",
     "KeyNotExistsException",
