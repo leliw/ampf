cbf657f0fabe1dd87515646a07e4bde4ac74d81c
Marcin Leliwa <marcin.leliwa@gmail.com>
2025-09-30 14:04:37 +0200

    feat(GcpPubsubPushEmulator): Add wait_for method

--- ampf/gcp/gcp_pubsub_push_emulator.py
+++ ampf/gcp/gcp_pubsub_push_emulator.py
@@ -64,6 +64,14 @@ class GcpPubsubPushEmulator[T: BaseModel]:
                     pass
 
     def isfinished(self, timeout: float = 60.0, expected_responses: int = 1) -> bool:
+        """Checks if the emulator has finished processing messages.
+
+        Args:
+            timeout: The maximum time in seconds to wait for responses.
+            expected_responses: The number of expected responses.
+        Returns:
+            True if the expected number of responses have been received or timeout occurred, False otherwise.
+        """
         if time.time() >= self.start_time + timeout:
             self._log.error("Timeout while waiting for responses")
             return True
@@ -72,11 +80,36 @@ class GcpPubsubPushEmulator[T: BaseModel]:
             return True
         return False
 
-    def get_responses(self) -> List[Response]:
-        return self.responses
+    def wait_for(self, timeout: float = 60.0, expected_responses: int = 1) -> None:
+        """Waits until the expected number of responses are received or timeout occurs.
+
+        Args:
+            timeout: The maximum time in seconds to wait for responses.
+            expected_responses: The number of expected responses.
+        """
+        while not self.isfinished(timeout=timeout, expected_responses=expected_responses):
+            time.sleep(0.2)
 
     def get_messages(self) -> List[Message]:
+        """Returns the list of received messages.
+
+        Returns:
+            The list of received messages.
+        """
         return self.messages
 
     def get_payloads(self) -> List[T]:
+        """Returns the list of deserialized payloads.
+
+        Returns:
+            The list of deserialized payloads.
+        """
         return self.payloads
+
+    def get_responses(self) -> List[Response]:
+        """Returns the list of sent responses.
+
+        Returns:
+            The list of responses.
+        """
+        return self.responses
