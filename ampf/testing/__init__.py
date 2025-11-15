07d924c394a33aa2e3bdf08901ead88f880e3d7c
Marcin Leliwa <marcin.leliwa@gmail.com>
2025-11-15 09:46:46 +0100

    fix: Handle import errors

--- ampf/testing/__init__.py
+++ ampf/testing/__init__.py
@@ -1,22 +1,33 @@
 from .api_test_client import ApiTestClient
 from .container_factory import container_factory, container_network_factory, docker_client
 from .fixture_cloud_run_proxy_factory import cloud_run_proxy_factory
-from .mock_gcp_publish import mock_gcp_publish
-from .mock_method import mock_method, MockMethod
-from .pubsub_manager import PubSubManager, pubsub_manager
-from .subscription_factory import SubscriptionFactory, subscription_factory
+from .mock_method import MockMethod, mock_method
 
 __all__ = [
     "ApiTestClient",
-    "subscription_factory",
-    "SubscriptionFactory",
-    "mock_gcp_publish",
     "docker_client",
     "container_factory",
     "container_network_factory",
     "cloud_run_proxy_factory",
-    "pubsub_manager",
-    "PubSubManager",
     "mock_method",
     "MockMethod",
 ]
+
+
+try:
+    from .mock_gcp_publish import mock_gcp_publish
+    from .pubsub_manager import PubSubManager, pubsub_manager
+    from .subscription_factory import SubscriptionFactory, subscription_factory
+
+    __all__.extend(
+        [
+            "mock_gcp_publish",
+            "subscription_factory",
+            "SubscriptionFactory",
+            "pubsub_manager",
+            "PubSubManager",
+        ]
+    )
+
+except ImportError:
+    pass
--- ampf/testing/plugin.py
+++ ampf/testing/plugin.py
@@ -1,9 +1,17 @@
 try:
     # Check if testing module is installed
     import pytest_mock  # noqa: F401
-
     from .mock_method import mock_method  # noqa: F401
+except ImportError:
+    pass
+
+try:
     from .container_factory import container_factory, container_network_factory, docker_client  # noqa: F401
+    from .pubsub_manager import pubsub_manager, PubSubManager  # noqa: F401
+except ImportError:
+    pass
+
+try:
     from .pubsub_manager import pubsub_manager, PubSubManager  # noqa: F401
 except ImportError:
     pass
