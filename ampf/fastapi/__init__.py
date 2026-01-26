19b782e12eb33a3cbc7635772a8c8e41b47993cb
Marcin Leliwa <marcin.leliwa@gmail.com>
2026-01-26 08:48:06 +0100

    feat(fastapi): Add BlobStreamingResponse for efficient blob streaming
    - Introduce `BlobStreamingResponse` class to stream `Blob` objects
    - Utilize FastAPI's `StreamingResponse` for efficient data transfer
    - Automatically set `Content-Type` from `Blob` metadata
    - Allow optional `Cache-Control` header configuration

--- ampf/fastapi/__init__.py
+++ ampf/fastapi/__init__.py
@@ -1,5 +1,5 @@
+from .blob_streaming_response import BlobStreamingResponse
 from .json_straming_response import JsonStreamingResponse, StreamedException
 from .static_file_response import StaticFileResponse
 
-
-__all__ = ["JsonStreamingResponse", "StreamedException", "StaticFileResponse"]
+__all__ = ["JsonStreamingResponse", "StreamedException", "StaticFileResponse", "BlobStreamingResponse"]
